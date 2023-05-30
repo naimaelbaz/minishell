a73021189219820cb5a031428f4c1c20d37ef9a4
nel-baz <nnaimaelbaz@gmail.com>
2023-05-30 18:48:02 +0100

    first push to my branch

--- minishell.h
+++ minishell.h
@@ -6,11 +6,16 @@
 /*   By: nel-baz <nel-baz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 18:44:30 by nel-baz           #+#    #+#             */
-/*   Updated: 2023/05/30 18:45:01 by nel-baz          ###   ########.fr       */
+/*   Updated: 2023/05/30 18:47:34 by nel-baz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 #endif
